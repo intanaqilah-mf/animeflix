6abf9e560ee65aeb8d960a83b880b78d394afe70
Intan <intan@axtraction.ai>
2025-11-11 11:54:26 +0800

    chore: radix ui library implementation

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,17 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
+import { Theme } from '@radix-ui/themes';
+import '@radix-ui/themes/styles.css';
 import './index.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <Theme accentColor="violet" grayColor="mauve" radius="large" scaling="100%">
+        <App />
+      </Theme>
     </Provider>
   </StrictMode>
 );
